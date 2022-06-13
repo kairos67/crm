0c3d9952c865b5162828588e2f7464bb7f81c8ee
kairos67 <kairosys@gmail.com>
2022-06-13 20:15:44 +0900

    component, props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer ={
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'male',
+  'job':'Student'
+}
+class App extends Component {
+  render() {
+    return (
+      <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+      />
+    )
+  }
 }
 
 export default App;
